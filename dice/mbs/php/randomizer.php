5a0c6d4b0f74288b65b4620cdbb0f64430a456e4
Maksxpl <maks.kozon@gmail.com>
2022-07-02 01:25:56 +0200

    Update randomizer.php

--- dice/mbs/php/randomizer.php
+++ dice/mbs/php/randomizer.php
@@ -1,7 +1,7 @@
 <?php
 // goal was to get a random line from file, and save it, so we can fetch it later in js, or something like this, I think  
 
-$target_dir = "../../dice/data/";
+$target_dir = "../../../dice/data/";
 $target_file = $target_dir . basename("list.txt");
 
 // I feel like I'm doing something dumb, perhaps one day I'll look at this and fix it
